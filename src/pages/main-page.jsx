ef20aa1ad8fbac56f92fdef13674ff31f8ec1821
hong <daggax8235@gmail.com>
2024-04-08 15:17:30 +0900

    메인페이지 링크 추가

--- src/pages/main-page.jsx
+++ src/pages/main-page.jsx
@@ -1,7 +1,59 @@
-// 상훈님 작성 페이지
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const CenteredContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Button = styled.button`
+  margin: 10px;
+  padding: 8px 16px;
+  font-size: 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`
 
 const MainPage = () => {
-  return <div>main page</div>
+  return (
+    <CenteredContainer>
+      <Container>
+        <div>Main page</div>
+        <Link to='/apiTest'>
+          <Button>ApiTestPage</Button>
+        </Link>
+        <Link to='/'>
+          <Button>MainPage</Button>
+        </Link>
+        <Link to='/post/:postId/answer'>
+          <Button>AnswerPage</Button>
+        </Link>
+        <Link to='/post/:postId'>
+          <Button>PostIdPage</Button>
+        </Link>
+        <Link to='/list'>
+          <Button>ListPage</Button>
+        </Link>
+      </Container>
+    </CenteredContainer>
+  )
 }
 
 export default MainPage
