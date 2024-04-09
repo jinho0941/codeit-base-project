3c8f56076132d173e5074a9a46e99b3ccd15875e
임상훈 <sanghoon@MacBook-Pro-2.local>
2024-04-09 16:43:15 +0900

    GoAskButton 컴포넌트 디자인 작업 완료

--- src/component/main/GoAskButton.jsx
+++ src/component/main/GoAskButton.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import arrowRight from "../../arrow-right.svg";
+
+const StyledButton = styled.button`
+  width: 161px;
+  height: 46px;
+  padding: 12px 24px;
+  border-radius: 8px;
+  background: #f5f1ee;
+  color: #542f1a;
+  font-size: 16px;
+  border: 1px solid #542f1a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
 
 function GoAskButton() {
   return (
-    <Link to="/list">
-      <button>질문하러 가기</button>
-    </Link>
+    <StyledLink to="/list">
+      <StyledButton>
+        질문하러 가기
+        <img src={arrowRight} />
+      </StyledButton>
+    </StyledLink>
   );
 }
 export default GoAskButton;
