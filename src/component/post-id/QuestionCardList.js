4c1ccda3ec094aceba25c1e2e4a78da1be1a14f6
Homin <coc1381@naver.com>
2024-04-15 15:46:01 +0900

    질문이 없는 경우의 페이지 작성 완료

--- src/component/post-id/QuestionCardList.js
+++ src/component/post-id/QuestionCardList.js
@@ -1,17 +1,22 @@
-import QuestionCard from './QuestionCard';
+import QuestionCard from "./QuestionCard";
+import noQuestionImg from "../../images/post-id-images/no-question.svg";
 
 function QuestionCardList({ questions, profile }) {
   return (
     <>
-      {questions.map((question) => {
-        return (
-          <QuestionCard
-            key={question.id}
-            question={question}
-            profile={profile}
-          />
-        );
-      })}
+      {questions.length === 0 ? (
+        <img src={noQuestionImg} alt="no-question" />
+      ) : (
+        questions.map((question) => {
+          return (
+            <QuestionCard
+              key={question.id}
+              question={question}
+              profile={profile}
+            />
+          );
+        })
+      )}
     </>
   );
 }
