495a3a7158a9b9b966c17220b75f7d1684124af9
Homin <coc1381@naver.com>
2024-04-17 14:21:47 +0900

    BASE_URL 수정

--- src/api/post-id/post-api.js
+++ src/api/post-id/post-api.js
@@ -1,46 +1,41 @@
 import axios from "axios";
 
-const BASE_URL = "https://openmind-api.vercel.app/5-3/subjects/4840";
+const BASE_URL = "https://openmind-api.vercel.app/5-3";
 
 export async function getSubject() {
-  const response = await axios.get(`${BASE_URL}/`);
+  const response = await axios.get(`${BASE_URL}/subjects/4840/`);
 
   return response;
 }
 
 export async function getQuestion({ offset, limit }) {
   const query = `?offset=${offset}&limit=${limit}`;
-  const response = await axios.get(`${BASE_URL}/questions/${query}`);
+  const response = await axios.get(
+    `${BASE_URL}/subjects/4840/questions/${query}`
+  );
 
   return response;
 }
 
 export async function createQuestion(inputValue) {
-  const response = await axios.post(`${BASE_URL}/questions/`, {
+  const response = await axios.post(`${BASE_URL}/subjects/4840/questions/`, {
     content: inputValue,
+  });
+  return response;
+}
+
+export async function createLike(id) {
+  const response = await axios.post(`${BASE_URL}/questions/${id}/reaction/`, {
+    type: "like",
   });
 
   return response;
 }
 
-export async function createLike(id) {
-  const response = await axios.post(
-    `https://openmind-api.vercel.app/5-3/questions/${id}/reaction/`,
-    {
-      type: "like",
-    }
-  );
-
-  return response;
-}
-
 export async function createDislike(id) {
-  const response = await axios.post(
-    `https://openmind-api.vercel.app/5-3/questions/${id}/reaction/`,
-    {
-      type: "dislike",
-    }
-  );
+  const response = await axios.post(`${BASE_URL}/questions/${id}/reaction/`, {
+    type: "dislike",
+  });
 
   return response;
 }
