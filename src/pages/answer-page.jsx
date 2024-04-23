bab88018be5a0a95939baf1e7a1ffa4231de9a0c
daggax <daggax8235@gmail.com>
2024-04-23 14:23:10 +0900

    로컬스토리지 값 삭제

--- src/pages/answer-page.jsx
+++ src/pages/answer-page.jsx
@@ -1,16 +1,16 @@
-import { useNavigate, useParams } from "react-router-dom";
-import useSubjectIdData from "../hooks/use-subject-id-data";
-import useQuestionsData from "../hooks/use-questions-data";
-import useScrollEvent from "../hooks/use-scroll-event";
-import { useEffect, useState } from "react";
-import api from "../utils/api";
-import Container, { ButtonWrapper } from "../component/answer/Container";
+import { useNavigate, useParams } from 'react-router-dom'
+import useSubjectIdData from '../hooks/use-subject-id-data'
+import useQuestionsData from '../hooks/use-questions-data'
+import useScrollEvent from '../hooks/use-scroll-event'
+import { useEffect, useState } from 'react'
+import api from '../utils/api'
+import Container, { ButtonWrapper } from '../component/answer/Container'
 
-import { createGlobalStyle } from "styled-components";
-import Background from "../component/post-id/Background";
-import UserInfo from "../component/answer/UserInfo";
-import ContentsFrame from "../component/answer/ContentsFrame";
-import styled from "styled-components";
+import { createGlobalStyle } from 'styled-components'
+import Background from '../component/post-id/Background'
+import UserInfo from '../component/answer/UserInfo'
+import ContentsFrame from '../component/answer/ContentsFrame'
+import styled from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -20,7 +20,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
   }
-`;
+`
 
 const DeleteButton = styled.button`
   margin-right: 0;
@@ -36,37 +36,42 @@ const DeleteButton = styled.button`
   background: var(--Brown-40, #542f1a);
   color: var(--Grayscale-10, #fff);
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-`;
+`
 
 function AnswerPage() {
-  const params = useParams();
-  const postId = params.postId;
+  const params = useParams()
+  const postId = params.postId
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const { subjectIdData, isLoading } = useSubjectIdData(postId);
-  const { offset, setOffset } = useQuestionsData(postId);
+  const { subjectIdData, isLoading } = useSubjectIdData(postId)
+  const { offset, setOffset } = useQuestionsData(postId)
 
-  const [currentOffset, setCurrentOffset] = useState(offset);
+  const [currentOffset, setCurrentOffset] = useState(offset)
 
   useEffect(() => {
-    setOffset(currentOffset);
-  }, [currentOffset]);
+    setOffset(currentOffset)
+  }, [currentOffset])
 
   useScrollEvent(() => {
-    setCurrentOffset((value) => value + 5);
-  });
+    setCurrentOffset((value) => value + 5)
+  })
 
-  if (!subjectIdData) return null;
+  if (!subjectIdData) return null
 
   const deleteSubject = async () => {
     try {
-      await api.delete(`/subjects/${postId}/`);
-      navigate("/list");
+      await api.delete(`/subjects/${postId}/`)
+      localStorage.removeItem('questionId')
+
+      navigate('/list')
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
+  if (!subjectIdData) {
+    return navigate('/')
+  }
 
   return (
     <>
@@ -80,7 +85,7 @@ function AnswerPage() {
         <ContentsFrame />
       </Container>
     </>
-  );
+  )
 }
 
-export default AnswerPage;
+export default AnswerPage
